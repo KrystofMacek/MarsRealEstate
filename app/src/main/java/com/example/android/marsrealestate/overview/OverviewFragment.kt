03a2a70ae51d0cb13dd2b2f02d786cebf070cd7c
Krystof <krystofmacek333@gmail.com>
2020-06-29 13:04:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -0,0 +1,37 @@
+
+
+package com.example.android.marsrealestate.overview
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.marsrealestate.R
+import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
+
+class OverviewFragment : Fragment() {
+
+
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProviders.of(this).get(OverviewViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+}
